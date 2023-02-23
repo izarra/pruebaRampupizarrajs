00cd0891ce9c24a44ba67032ca848211f2e57585
izarra <31973220+izarra@users.noreply.github.com>
2023-02-23 10:09:33 +0100

    Add files via upload

---
+++ pruebajs/pruebaIzarra.js
@@ -0,0 +1,15 @@
+for {(let i=0; i<10;i++)}/*ejercicio1*/
+array = 1
+for {(let array<10;i++)}/*ejercicio2*/
+/*Crea una función que reciba un array con las edades de los clientes y devuelva un array con los que tienen la edad mínima.
+Ejemplo: comprobarEdad([15, 18, 20, 30]) devolverá [18, 20, 30]
+Si no hay nadie con la edad mínima, la función devolverá un array vacío.
+Ejemplo: comprobarEdad([15, 16, 17]) devolverá []
+Si no sabes cómo devolver el array, muestra por pantalla cada elemento que cumpla la condición.
+
+Ejercicio3*/
+if (comprobaEdad[15, 18, 20, 30]) {
+    (let comprobaEdad<18)
+} else { (let comprobaEdad>= 18)
+    
+}
